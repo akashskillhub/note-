d23127826a8e01c8034a59b8d229637aa80c29fc
akash@maticui <akash@maticui.com>
2025-06-21 19:33:41 +0530

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("dotenv").config()
 const cors = require("cors")
 const app = express()
 app.use(express.json())
-app.use(cors({ origin: "http://localhosta:5173", credentials: true }))
+app.use(cors({ origin: "http://localhost:5173", credentials: true }))
 app.use("/api/notes", require("./routes/note.route"))
 
 mongoose.connect(process.env.MONGO_URL)
